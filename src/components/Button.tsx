a60806e851d59d21bdf1df5bdba325bab5a1a95e
Mikoyzskie <rapidstrike13@gmail.com>
2024-08-11 20:18:10 +0800

    chore: + public files, header component

---
+++ src/components/Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Button() {
+    return (
+        <button className='bg-gradient-to-r from-[#33D35E] to-[#2AB6D9] py-2 px-8 rounded-full'>
+            <span className='font-bold text-sm text-white'>
+                Request Invite
+            </span>
+        </button>
+    )
+}
---
+++ src/components/Header.tsx
@@ -0,0 +1,31 @@
+import styles from "@/app/styles/common.module.css"
+import Image from "next/image"
+import Button from "./Button"
+
+export default function Header() {
+
+    const links = ["Home", "About", "Contact", "Blog", "Careers"]
+
+    return (
+        <div className="bg-white">
+            <header className={`${styles.maxWidth} py-[18px] flex justify-between items-center `}>
+                <Image
+                    src={"logo.svg"}
+                    height={20}
+                    width={129}
+                    alt="Header Logo"
+                />
+                <ul className="flex gap-[30px]">
+                    {
+                        links.map((link, index) => {
+                            return (
+                                <li key={index} className="text-[#9597A5] text-sm">{link}</li>
+                            )
+                        })
+                    }
+                </ul>
+                <Button />
+            </header>
+        </div>
+    )
+}
